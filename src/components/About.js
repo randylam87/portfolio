a2aeb34570831488f5f193e52073428f776b6ee9
Randy <randylam87@gmail.com>
2017-08-15 17:35:48 -0700

    added image to about page, added some text in my description

--- src/components/About.js
+++ src/components/About.js
@@ -1,21 +1,36 @@
 import React, { Component } from 'react';
-import { Container } from 'reactstrap';
-
+import { Container, Col, Row } from 'reactstrap';
+import Me from '../me.png';
 
 class About extends Component {
   render() {
     return (
         <div className='page text-center d-flex align-items-center' id='about'>
           <Container>
-            <h1>About Me</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus assumenda, nulla minima, molestiae explicabo molestias laboriosam excepturi. Accusantium ab in, dolorem distinctio corporis at libero adipisci, placeat laudantium debitis molestias.
-            </p>
-            <p>Molestias, fugiat distinctio quo assumenda explicabo quisquam. Labore vitae explicabo hic sequi nisi sit dolor quam, natus, maiores iure quia, nihil dolore cum laboriosam omnis ad nesciunt commodi voluptates, veritatis.
-            </p>
-            <p>Nostrum nam assumenda, velit nobis quis at quisquam accusamus, cum quo sunt enim unde laudantium officiis deleniti et! Facere molestias odit doloribus libero vel dolores veniam minima quas nobis accusantium.
-            </p>
-            <p>Reprehenderit, fuga a. Sequi quas qui fugit molestiae quasi ratione reiciendis perferendis unde vitae enim et nesciunt doloribus quia, repudiandae ea fuga. Magnam, ratione suscipit nobis facere ab ducimus ipsa!
-            </p>
+            <Row>
+              <h1><strong>About Me</strong></h1>
+            </ Row>
+            
+              <Row>
+              <h4>Full Stack Web Developer</h4>
+              </ Row>
+              <Row>
+              <Col xs={6} id='about-img' >
+                <img src={Me} className='img-thumbnail text-center'  alt='Self Portrait'/>
+              </ Col>
+              <Col xs={6}>
+                <p> // Hello World!
+                </p>
+                <p>My name is Randy Lam and I am a health care professional turned programmer. I have worked within the pharmacy industry for the last 6+ years. Over the years, I have come to the realization that I have a strong drive to learn and problem solve and the pharmacy industry is not the ideal environment for my needs. Once I learned about programming, I knew this is the industry for me.
+                </p>
+                <p>Nostrum nam assumenda, velit nobis quis at quisquam accusamus, cum quo sunt enim unde laudantium officiis deleniti et! Facere molestias odit doloribus libero vel dolores veniam minima quas nobis accusantium.
+                </p>
+                <p>Reprehenderit, fuga a. Sequi quas qui fugit molestiae quasi ratione reiciendis perferendis unde vitae enim et nesciunt doloribus quia, repudiandae ea fuga. Magnam, ratione suscipit nobis facere ab ducimus ipsa!
+                </p>
+              </ Col>
+              </ Row>
+            
+            
           </ Container>
         </div>
       
